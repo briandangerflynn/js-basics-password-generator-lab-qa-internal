ec1cb99f5646640be389d58637e10ab47b99d6a8
Brian Flynn <bdflynny@gmail.com>
2019-01-31 23:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ window.addEventListener("load", () => {
   caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   capsArray = caps.split('')
   
+  lowers = ""
+  
   //start your code here
   const button = document.querySelector("#button")
   const output = document.querySelector("output")
