d7174905f3b1402876aaf8c066abf583d4f74d39
Brian Flynn <bdflynny@gmail.com>
2019-01-31 23:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", () => {
   //start your code here
   const button = document.querySelector("#button")
-  
+  button.addEventListener("click", () =>{
+    input = document.querySelector("#input")
+  })
 });
